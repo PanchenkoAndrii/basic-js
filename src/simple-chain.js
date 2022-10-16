0a280cc5554920321780a6736b7f196ae5138cc7
PanchenkoAndrii <panchenko057@gmail.com>
2022-10-16 18:33:47 +0300

    feat: add recursive-depth, simple-chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -5,28 +5,34 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 const chainMaker = {
-  getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+	arr: [],
+	getLength() {
+		return this.arr.length;
+	},
+	addLink(value) {
+		this.arr.push(`( ${value} )`);
+		return this;
+	},
+	removeLink(position) {
+		if (!Number.isInteger(position) || position < 1 || position > this.arr.length) {
+			this.arr = [];
+			throw new Error('You can\'t remove incorrect link!');
+		} else {
+			this.arr.splice(position - 1, 1);
+		}
+		return this;
+	},
+	reverseChain() {
+		this.arr.reverse();
+		return this;
+	},
+	finishChain() {
+		const arr = [...this.arr];
+		this.arr = [];
+		return arr.join('~~');
+	}
 };
 
 module.exports = {
-  chainMaker
+	chainMaker
 };
