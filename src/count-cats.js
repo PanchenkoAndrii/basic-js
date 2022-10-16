6221d60288a1183d0503f0182381fb610bfc8d48
PanchenkoAndrii <panchenko057@gmail.com>
2022-10-16 14:29:50 +0300

    feat: add counts-cats, what-season

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,11 +14,16 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function countCats(matrix) {
+	let count = 0;
+	for (let cat of matrix) {
+		if (Array.isArray(cat)) {
+			count = count + countCats(cat);
+		}
+		if (cat === '^^') { count++; }
+	}
+	return count;
 }
-
 module.exports = {
-  countCats
+	countCats
 };
