a5aad114d1b31f70cce89f69ed660c647807adf5
PanchenkoAndrii <panchenko057@gmail.com>
2022-10-16 19:18:55 +0300

    feat: add dns-stats, extended-repeater, file-names

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,11 +15,29 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+	str = String(str);
+	options = options !== undefined ? options : {};
+	repeatTimes = options.repeatTimes === undefined ? 1 : Number(options.repeatTimes)
+	separator = options.separator === undefined ? '+' : String(options.separator)
+	addition = options.addition === undefined ? '' : `${options.addition}`
+	additionRepeatTimes = options.additionRepeatTimes === undefined ? 1 : Number(options.additionRepeatTimes)
+	additionSeparator = options.additionSeparator === undefined ? '|' : options.additionSeparator
+
+	let add = [];
+	for (let i = 0; i < additionRepeatTimes; i++) {
+		if (addition != '') add.push(addition);
+	}
+
+	if (add.length != 0) add = add.join(additionSeparator);
+	let s = [];
+	for (let i = 0; i < repeatTimes; i++) {
+		s.push(str + add);
+	}
+	if (s.length != 0 && typeof s != 'string') s = s.join(separator);
+	return s;
 }
 
 module.exports = {
-  repeater
+	repeater
 };
